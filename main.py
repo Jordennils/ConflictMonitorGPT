fe17b0a0e0a30a15b60deed1fb18a2b6dc3407f1
Jordennils <frissenjorden@gmail.com>
2025-06-23 12:53:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from telegram import Bot
 bot = Bot(token=os.environ.get("TELEGRAM_BOT_TOKEN"))
 chat_id = os.environ.get("TELEGRAM_CHAT_ID")
 
-def send_test_message():
+def #send_test_message():
     bot.send_message(chat_id=chat_id, text="✅ TEST: Melding ontvangen! Bot werkt correct.")
 
 if __name__ == "__main__":
-    send_test_message()
+    #send_test_message()
